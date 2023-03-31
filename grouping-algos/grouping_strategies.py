35ca128e02782b11247cc13a6ab91f29890c37ec
kelvingakuo <kelvingakuo@gmail.com>
2023-03-31 12:10:39 +0300

    Implement average and median

--- grouping-algos/grouping_strategies.py
+++ grouping-algos/grouping_strategies.py
@@ -1,62 +1,64 @@
 import pprint
 import json
+import statistics
+# TODO: Implement aggregating multiple columns
+# TODO: Implement multiple aggregations on a single column
 
-def calc_aggregate(val_a, val_b, how):
-    """ Does a calculation between val_a and val_b based on how
+def calc_aggregate(vals, how):
+    """ Does a calculation on a list of values based on how
     """
     if(how == "SUM"):
-        return val_a + val_b
+        return sum(vals)
     elif(how == "AVG"):
-        return (val_a + val_b)/2
+        return statistics.mean(vals)
     elif(how == "MIN"):
-        return min(val_a, val_b)
+        return min(vals)
     elif(how == "MAX"):
-        return max(val_a, val_b)
+        return max(vals)
     elif(how == "MEDIAN"):
+        return statistics.median(vals)
+    elif(how == "COUNT"):
+        # TODO: Impelement COUNT
         pass
     else:
         raise NotImplementedError(f"The aggregation {how} is not implemented. Try SUM, AVG, MIN, MAX, MEDIAN")
 
 class GroupingAlgos(object):
-    def __init__(self, data, on_cols, agg_col, agg, algorithm) -> None:
+    def __init__(self, data, on_cols, agg_col, agg) -> None:
         """ Accepts the data, the column(s) to group on and the aggregation to do. Then calls the specified algorithm
 
         Params:
-            data - List of dictionaries representing rows
+            data - List of dictionaries representing rows of data
             on_cols - List of the column(s) to group on
             agg_col - The column to aggregate
-            agg - List of aggregations to perform. Accepts: SUM, AVG, MIN, MAX, MEDIAN
-            algorithm - The grouping algorithm to use. Accepts: StreamAggregate, HashAggregate, MixedAggregate, PartialAggregate
+            agg - The aggregation to perform. Accepts: SUM, AVG, MIN, MAX, MEDIAN
         """
         self.rows = data
         self.on = on_cols
         self.agg_col = agg_col
         self.agg = agg
 
-        if(algorithm == "StreamAggregate"):
-            self.streaming_aggregate()
-        elif(algorithm == "HashAggregate"):
-            self.hashing_aggregate()
-        elif(algorithm == "MixedAggregate"):
-            self.mixed_aggregate()
-        elif(algorithm == "PartialAggregate"):
-            self.partial_aggregate()
-
     def streaming_aggregate(self):
         sorted_rows = sorted(self.rows, key = lambda r: [r[k] for k in self.on])
-        with open("sorttt.json", "w") as fp:
+        with open("sorteddd.json", "w") as fp:
             json.dump(sorted_rows, fp)
-        
+
+
         i = 0
+        if self.agg in ("AVG", "MEDIAN"): # For avg and median, we need to calculate on the entire list of values in the group
+            cache_vals = [] 
         while i <= len(sorted_rows):
             if(i == 0):
                 agg_value = sorted_rows[0][self.agg_col]
+                if self.agg in ("AVG", "MEDIAN"): # Add the first value to the list to finally agg on
+                    cache_vals.append(agg_value)
             elif(i == len(sorted_rows)):
                 prev_row = sorted_rows[i - 1]
                 prev_values = list(map(prev_row.get, self.on))
-                print(prev_values)
-                print(agg_value)
-                print("----")
+                if(self.agg in ("AVG", "MEDIAN")): # Calculate avg or median before yielding
+                    agg_value = calc_aggregate(cache_vals, self.agg)
+
+                yield {"group": prev_values, f"group_{self.agg}": agg_value}
             else:
                 this_row = sorted_rows[i]
                 prev_row = sorted_rows[i - 1]
@@ -66,13 +68,20 @@ class GroupingAlgos(object):
 
 
                 if(this_values == prev_values):
-                    agg_value = calc_aggregate(this_row[self.agg_col], agg_value, self.agg[0])
+                    if(self.agg in ("AVG", "MEDIAN")):
+                        cache_vals.append(this_row[self.agg_col]) # Add to list
+                    else:
+                        agg_value = calc_aggregate([this_row[self.agg_col], agg_value], self.agg) # Calculate
                 elif(this_values != prev_values):
-                    print(prev_values)
-                    # print(this_values)
-                    print(agg_value)
-                    print("----")
-                    agg_value = this_row[self.agg_col]                    
+                    if(self.agg in ("AVG", "MEDIAN")):  # Calculate avg or median before yielding
+                        agg_value = calc_aggregate(cache_vals, self.agg)
+                        cache_vals = []
+                    
+                    yield {"group": prev_values, f"group_{self.agg}": agg_value}
+
+                    agg_value = this_row[self.agg_col]
+                    if self.agg in ("AVG", "MEDIAN"):
+                        cache_vals.append(agg_value)                    
             i = i + 1
     
 
--- grouping-algos/test_grouping.py
+++ grouping-algos/test_grouping.py
@@ -5,13 +5,37 @@ import random
 fake = Faker()
 Faker.seed(5)
 methods = ["mpesa", "card", "cash"] #List of payment methods
-businesses = [fake.company() for _ in range(10)] #List of random business names
+businesses = [f"{fake.name()} {fake.company_suffix()}" for _ in range(2)] #List of random business names
 
 test_data = [] # Final list of dictionaries
 for _ in range(10): # Generate 10000 random combinations
-    row = {"business_name": random.choice(businesses), "payment_method": random.choice(methods), "amount": random.randint(10, 100)}
+    row = {"business_name": random.choice(businesses), "payment_method": random.choice(methods), "amount": random.randint(random.randint(1, 10), random.randint(100, 1000))}
     test_data.append(row)
 
+grouper_sum = GroupingAlgos(test_data, on_cols = ["business_name"], agg_col = "amount", agg = "SUM")
+grouper_max = GroupingAlgos(test_data, on_cols = ["business_name"], agg_col = "amount", agg = "MAX")
+grouper_min = GroupingAlgos(test_data, on_cols = ["business_name"], agg_col = "amount", agg = "MIN")
+grouper_avg = GroupingAlgos(test_data, on_cols = ["business_name"], agg_col = "amount", agg = "AVG")
+grouper_med = GroupingAlgos(test_data, on_cols = ["business_name"], agg_col = "amount", agg = "MEDIAN")
 
-grouped_output = GroupingAlgos(test_data, on_cols = ["business_name", "payment_method"], agg_col = "amount", agg = ["SUM"], algorithm = "StreamAggregate")
+grouper_sum_output = grouper_sum.streaming_aggregate()
+grouper_max_output = grouper_max.streaming_aggregate()
+grouper_min_output = grouper_min.streaming_aggregate()
+grouper_avg_output = grouper_avg.streaming_aggregate()
+grouper_med_output = grouper_med.streaming_aggregate()
 
+for group in grouper_sum_output:
+    print(group)
+print("-------------------")
+for group in grouper_max_output:
+    print(group)
+print("-------------------")
+for group in grouper_min_output:
+    print(group)
+print("-------------------")
+for group in grouper_avg_output:
+    print(group)
+print("-------------------")
+for group in grouper_med_output:
+    print(group)
+print("-------------------")
